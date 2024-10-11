a4274e88dbeedf5b982ff6e9a0363757085b3140
Poojashreekamalaksha <poojakamalaksha98@gmail.com>
2024-10-11 12:50:32 +0530

    09/10/2024_Constant

---
+++ src/October/ex_09102024/Lab0012_constant.java
@@ -0,0 +1,67 @@
+package October.ex_09102024;
+
+public class Lab0012_constant {
+    public static final double PI=3.142;
+    public static void main(String[] args) {
+        int radius = 5;
+        double area = PI * radius * radius;
+        System.out.println("The area of the circle is  "+ area);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
