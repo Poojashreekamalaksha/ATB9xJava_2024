c7858724d780e1dbcfa3323051a74e011e523aac
Poojashreekamalaksha <poojakamalaksha98@gmail.com>
2024-10-17 15:08:06 +0530

    15/10/2024

--- src/Task/task_15102024/Lab003_IncrementDecrement.java
+++ src/Task/task_15102024/Lab003_IncrementDecrement.java
@@ -3,11 +3,12 @@ package Task.task_15102024;
 public class Lab003_IncrementDecrement {
     public static void main(String[] args) {
         int a = 20;
-        System.out.println(--a + a++ + a--);//18
-        System.out.println(a);//8
-        //a--=10   a=9
-        //--a=8   a=8
-
+        System.out.println(--a + a++ + a--);//58
+        System.out.println(a);//19
+        //--a=19    a=19
+        //a++=19  a=20
+        //a--=20   a=19
+        //58       //19
     }
 }
 
